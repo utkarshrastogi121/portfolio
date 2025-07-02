114c6469f9b641047c00cecf37fb618647b4bc66
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-07-02 21:41:56 +0530

    added link to portfolio on welcom page

--- src/Pages/WelcomeScreen.jsx
+++ src/Pages/WelcomeScreen.jsx
@@ -177,7 +177,7 @@ const WelcomeScreen = ({ onLoadingComplete }) => {
                 data-aos-delay="1200"
               >
                 <a
-                  href="https://www.abc.com/"
+                  href="https://utk-portfolio.onrender.com/"
                   className="inline-flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 rounded-full relative group hover:scale-105 transition-transform duration-300"
                   target="_blank"
                   rel="noopener noreferrer"
