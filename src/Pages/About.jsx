b5845b34b7f48133316ada05591e1709367bd7c0
Utkarsh Rastogi <145201743+utkarshrastogi121@users.noreply.github.com>
2025-08-30 00:25:48 +0530

    Update resume link in About page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -247,7 +247,7 @@ const AboutPage = () => {
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
               <a
-                href="https://drive.google.com/file/d/1VMT595-MlV7MGXkLdyTKItV4nkgaSqBQ/view"
+                href="https://drive.google.com/file/d/1VlqlZsO5XD8gzzETVh0NWNh6Jt5EokwX/view"
                 className="w-full lg:w-auto"
               >
                 <button
