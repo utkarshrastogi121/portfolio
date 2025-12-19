1717029b6c0ba333362b6f22c925b7835dcfe3f6
Utkarsh Rastogi <145201743+utkarshrastogi121@users.noreply.github.com>
2025-12-19 17:52:28 +0530

    Updated resume link

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -247,7 +247,7 @@ const AboutPage = () => {
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
               <a
-                href="https://drive.google.com/file/d/1VlqlZsO5XD8gzzETVh0NWNh6Jt5EokwX/view"
+                href="https://drive.google.com/file/d/112h0f_ot0_iQkxmXnrBUg98vn2RERo6h/view?usp=drive_link"
                 className="w-full lg:w-auto"
               >
                 <button
